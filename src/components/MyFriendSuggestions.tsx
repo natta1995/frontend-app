34307e7a764b225c134202b36d388fc6cd5ce7ed
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-10-22 14:36:12 +0200

    Carousel added + css update

--- src/components/MyFriendSuggestions.tsx
+++ src/components/MyFriendSuggestions.tsx
@@ -1,10 +1,9 @@
 import { useUser } from "../UserContext";
 import { styled } from "styled-components";
-import { Button } from "react-bootstrap";
+import { Button, Carousel, Card } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import ProfileImg from "../Img/startimg.webp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Card } from "react-bootstrap";
 import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 
@@ -17,6 +16,29 @@ const BoxContainer = styled.div`
   background-color: #faedcd;
   margin-right: 15px;
   width: 100%;
+`;
+
+// Styling for the carousel arrows
+const StyledCarousel = styled(Carousel)`
+  .carousel-control-prev,
+  .carousel-control-next {
+    width: 5%;
+  }
+
+  .carousel-control-prev-icon,
+  .carousel-control-next-icon {
+    background-color: #d4a373;
+    border-radius: 50%;
+    padding: 10px;
+  }
+
+  .carousel-control-prev {
+    left: -60px; // Move left arrow further left
+  }
+
+  .carousel-control-next {
+    right: -60px; // Move right arrow further right
+  }
 `;
 
 const FriendSuggestions = () => {
@@ -99,86 +121,101 @@ const FriendSuggestions = () => {
 
       const randomUsers = nonFriends
         .sort(() => 0.4 - Math.random())
-        .slice(0, 4);
+        .slice(0, 8);
 
       setSuggestedUsers(randomUsers);
     }
   }, [friends, users, currentUser]);
 
+  const chunkedUsers = [];
+  for (let i = 0; i < suggestedUsers.length; i += 4) {
+    chunkedUsers.push(suggestedUsers.slice(i, i + 4));
+  }
+
   return (
     <BoxContainer>
       {error && <p>{error}</p>}
-      <ul style={{ margin: "10px", padding: "10px" }}>
-        <h6>Vänförslag - du kanske också känner?</h6>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            flexWrap: "wrap",
-            marginLeft: "5px",
-          }}
-        >
-          {suggestedUsers.map((user) => (
-            <Card
-              key={user.id}
+
+      <StyledCarousel>
+        {chunkedUsers.map((userGroup, index) => (
+          <Carousel.Item key={index}>
+            <div
               style={{
-                listStyle: "none",
-                padding: "2%",
-                alignItems: "center",
-                backgroundColor: "#fefae0",
-                cursor: "pointer",
-                margin: "10px",
-                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-                transition: "transform 0.2s",
-                width: "180px",
-                borderRadius: "10px",
+                display: "flex",
+                gap: "5%",
+                flexWrap: "wrap",
               }}
-              onMouseEnter={(e) => {
-                e.currentTarget.style.transform = "scale(1.05)"; // Zooma in kortet lite vid hover
-              }}
-              onMouseLeave={(e) => {
-                e.currentTarget.style.transform = "scale(1)"; // Återställ vid hover-out
-              }}
-              onClick={() => navigate(`/profile/${user.username}`)}
             >
-              <img
-                src={
-                  user.profile_image
-                    ? `http://localhost:1337${user.profile_image}`
-                    : ProfileImg
-                }
-                alt={`${user.username}s profile`}
-                style={{
-                  width: "100px",
-                  height: "100px",
-                  borderRadius: "50%",
-                  objectFit: "cover",
-                }}
-              />
-              <div style={{ textAlign: "center" }}>
-                {user.name}
-                <div>
-                  {sentRequests.includes(user.id) ? (
-                    <span style={{ marginLeft: "10px", color: "green" }}>
-                      Skickad
-                    </span>
-                  ) : (
-                    <Button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        sendFriendRequest(user.id);
-                      }}
-                      style={{ marginLeft: "10px" }}
-                    >
-                      <FontAwesomeIcon icon={faUserPlus} />
-                    </Button>
-                  )}
-                </div>
-              </div>
-            </Card>
-          ))}
-        </div>
-      </ul>
+              {userGroup.map((user) => (
+                <Card
+                  key={user.id}
+                  style={{
+                    listStyle: "none",
+                    padding: "2%",
+                    alignItems: "center",
+                    backgroundColor: "#fefae0",
+                    cursor: "pointer",
+                    margin: "10px",
+                    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                    transition: "transform 0.2s",
+                    width: "180px",
+                    borderRadius: "10px",
+                  }}
+                  onMouseEnter={(e) => {
+                    e.currentTarget.style.transform = "scale(1.05)";
+                  }}
+                  onMouseLeave={(e) => {
+                    e.currentTarget.style.transform = "scale(1)";
+                  }}
+                  onClick={() => navigate(`/profile/${user.username}`)}
+                >
+                  <img
+                    src={
+                      user.profile_image
+                        ? `http://localhost:1337${user.profile_image}`
+                        : ProfileImg
+                    }
+                    alt={`${user.username}s profile`}
+                    style={{
+                      width: "100px",
+                      height: "100px",
+                      borderRadius: "50%",
+                      objectFit: "cover",
+                    }}
+                  />
+                  <div style={{ textAlign: "center" }}>
+                    <strong>{user.name}</strong>
+                    <br />
+                    {user.username}
+                    <div>
+                      {sentRequests.includes(user.id) ? (
+                        <span style={{ marginLeft: "10px", color: "green" }}>
+                          Skickad
+                        </span>
+                      ) : (
+                        <Button
+                          onClick={(e) => {
+                            e.stopPropagation(); // Förhindrar att kortets onClick triggas
+                            sendFriendRequest(user.id);
+                          }}
+                          style={{
+                            marginLeft: "10px",
+                            backgroundColor: "#606c38",
+                            borderColor: "#606c38",
+                          }}
+                        >
+                          Lägg till vän
+                          <FontAwesomeIcon icon={faUserPlus} />
+                        </Button>
+                      )}
+                    </div>
+                  </div>
+                </Card>
+              ))}
+            </div>
+          </Carousel.Item>
+        ))}
+      </StyledCarousel>
     </BoxContainer>
   );
 };
