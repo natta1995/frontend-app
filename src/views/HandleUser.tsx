93657352e97f6879904a63926340429ed1075d79
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2024-10-28 16:25:55 +0100

    Update

--- src/views/HandleUser.tsx
+++ src/views/HandleUser.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 
 
 const HandleUser = () =>
 {
   return (
     <div style={{textAlign: "center", paddingTop: "5%"}}>
-    <h2>Vill du säga upp ditt konto?</h2>
-    <p>Du vet väll att du när som helst kan radera ditt konto.</p>
+    <h2>Vill du ta bort ditt konto?</h2>
+    <p>Du vet väll att du när som helst kan ta bort ditt konto?</p>
+    <p>OBS: Ett raderat konto går inte att återfå.</p>
+    <Button variant="danger">Radera Konto</Button>
     </div>
   )
 }
